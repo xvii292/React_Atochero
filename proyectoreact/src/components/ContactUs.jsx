0a884b0d810caea12f2d44bcf3629276afd54a03
xvii292 <130442788+xvii292@users.noreply.github.com>
2024-01-29 13:29:33 +0100

    XAVI
    Primer avance proyecto REACT

---
+++ proyectoreact/src/components/ContactUs.jsx
@@ -0,0 +1,40 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+export const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_68q5wqt",
+        "template_cfalc4q",
+        form.current,
+        "RGWahj2K0JGevqo5L"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail} class="form-restrictive">
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
+
+export default ContactUs;
